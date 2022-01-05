d4eee276645b5ef475b6eb687d890f427f3f170f
tyssejc <tyssejc@gmail.com>
2022-01-04 21:53:21 -0500

    personalizing (thanks Sven!)

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">ctysse.net</h1>
+      <h2 class="subtitle text-center">figuring out analytics engineering together</h2>
     </section>
 
     <section class="mt-8">
